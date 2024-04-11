83d8fda5a1bc5b50ddda57b7cf9c35beb8c88cd1
CLF <125818668+CLF233@users.noreply.github.com>
2024-04-12 00:39:12 +0800

    a

--- func/termux.sh
+++ func/termux.sh
@@ -2,7 +2,61 @@
 #By CLF
 #The Shawshank Redemption
 
-termux_feat(){
-  echo_blue "Developing..."
-  exit 0
+termux_feat() {
+	echo_blue "Developing...\n"
+	PROMPT="1. Container feats\n"
+	PROMPT+="0. Go to last page\n"
+	echo -e $PROMPT
+	get_input "Input Your choice: " "Input"
+	case $Input in
+	1)
+		container_menu
+		;;
+	0)
+		main
+		;;
+	*)
+		echo_red "E: Bad input: $Input"
+		termux_feat
+		;;
+	esac
 }
+
+container_menu() {
+	PROMPT="CLFTools container_menu\n"
+	PROMPT+="1. Download rootfs\n"
+	PROMPT+="0. Go to last page\n"
+	echo -e "${PROMPT}"
+	get_input "Input Your choice: " "Input"
+	case $Input in
+	1)
+		distro_download
+		;;
+	0)
+		termux_feat
+		;;
+	*)
+		echo_red "E: Bad input: $Input"
+		container_menu
+		;;
+	esac
+}
+
+distro_download() {
+	get_input "Input a distro: " DISTRO
+	LXC_MIRROR="https://mirrors.bfsu.edu.cn/lxc-images"
+	# Get rootfs download link of Specified $DISTRO and $VERSION
+	# $MIRROR, $DISTRO, $VERSION and $CPU_ARCH are defined at main()
+	if [[ ${CPU_ARCH} == "" ]]; then
+		get_cpu_arch
+	fi
+	DISTROPATH=$(curl -sL "${LXC_MIRROR}/meta/1.0/index-system" | grep ${DISTRO} | grep ${CPU_ARCH} | grep -v cloud | tail -n 1 | cut -d ";" -f 6)
+	ES=$?
+	if [[ $ES != 0 ]]; then
+		echo_red "E: Get link failed. Make sure you entered true distro name."
+		distro_download
+	fi
+	rm -f rootfs.tar.xz
+	wget "${LXC_MIRROR}${DISTROPATH}rootfs.tar.xz"
+}
+# Eh, I have to say that this is a script with full of shit ;(
--- main.sh
+++ main.sh
@@ -13,9 +13,46 @@ echo_red() {
 	echo -e "\033[31m${1}\033[0m"
 }
 
+# input func
+Input=""
+get_input(){
+  read -p "$1" $2
+}
+
 # TEMP folder
 TMP="/tmp/CLF${RAMDOM}"
 mkdir -p ${TMP}
+
+# Arch getter
+# It will create a global variable CPU_ARCH
+# From tmoe
+if [[ $(command -v dpkg) && $(command -v apt-get) ]]; then
+	DPKG_ARCH=$(dpkg --print-architecture)
+	case ${DPKG_ARCH} in
+	armel) ARCH_TYPE="armel" ;;
+	armv7* | armv8l | armhf | arm) ARCH_TYPE="armhf" ;;
+	aarch64 | arm64* | armv8* | arm*) ARCH_TYPE="arm64" ;;
+	i*86 | x86) ARCH_TYPE="i386" ;;
+	x86_64 | amd64) ARCH_TYPE="amd64" ;;
+	*) ARCH_TYPE=${DPKG_ARCH} ;;
+	esac
+else
+	UNAME_ARCH=$(uname -m)
+	case ${UNAME_ARCH} in
+	armv7* | armv8l) ARCH_TYPE="armhf" ;;
+	armv[1-6]*) ARCH_TYPE="armel" ;;
+	aarch64 | armv8* | arm64 | arm*) ARCH_TYPE="arm64" ;;
+	x86_64 | amd64) ARCH_TYPE="amd64" ;;
+	i*86 | x86) ARCH_TYPE="i386" ;;
+	s390*) ARCH_TYPE="s390x" ;;
+	ppc*) ARCH_TYPE="ppc64el" ;;
+	mips64) ARCH_TYPE="mips64el" ;;
+	mips*) ARCH_TYPE="mipsel" ;;
+	risc*) ARCH_TYPE="riscv64" ;;
+	*) ARCH_TYPE=${UNAME_ARCH} ;;
+	esac
+fi
+export CPU_ARCH=${ARCH_TYPE}
 
 # Arg solver #TODO
 
@@ -26,8 +63,8 @@ main() {
 	PROMPT+="2. Linux fetures\n"
 	PROMPT+="0. Exit\n"
 	echo -e "${PROMPT}"
-	read -p "Input your choice(INT): " i
-	case ${i} in
+  get_input "Input Your choice: " "Input"
+	case $Input in
 	1)
 		source ${TEMP}/func/termux.sh
 		termux_feat
@@ -36,8 +73,13 @@ main() {
 		source func/linux.sh
 		linux_feat
 		;;
-	0) exit 0 ;;
-	*) echo_red "E: Incorrect input: ${i}\n" ;;
+	0)
+		exit 0
+		;;
+	*)
+		echo_red "E: Bad input: $Input\n"
+		main
+		;;
 	esac
 }
 
